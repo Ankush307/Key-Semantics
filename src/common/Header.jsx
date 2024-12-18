4864e408caa357ab0abae6fcab9c169054cb5498
Ankush307 <ankushdhull966@gmail.com>
2024-12-18 14:53:02 +0530

    add a tag

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <div className="container mx-auto flex items-center justify-between md:pt-12 pt-6 max-xl:px-5">
-                <img className='max-sm:max-w-[180px]' src="./assets/image/webp/page-logo.webp" alt="page logo" />
+                <a href="/"><img className='max-sm:max-w-[180px]' src="./assets/image/webp/page-logo.webp" alt="page logo" /></a>
                 <CustomButton buttonText='Request a demo' myClass="bg-white max-sm:!text-xs" />
             </div>
         </>
